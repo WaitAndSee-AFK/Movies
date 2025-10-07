4e6c9a84545adf7530a85ae9de70dd55f1ff9efe
LELIK <b_banni@inbox.ru>
2025-10-07 10:23:55 +0300

    The MainViewModel and the necessary layouts have been created

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -6,21 +6,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
+
+private lateinit var viewModel: MainViewModel
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ApiFactory.apiService.loadMovies()
-            .observeOn(Schedulers.io())
-            .subscribeOn(AndroidSchedulers.mainThread())
-            .subscribe({ movies ->
-                Log.d("MainActivity", movies.toString())
-            }, { err ->
-                Log.d("MainActivity", err.toString())
-            })
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.movies.observe(this, object : Observer<List<Movie>>{
+            override fun onChanged(value: List<Movie>) {
+                Log.d(TAG, value.toString())
+            }
+        })
+        viewModel.loadMovies()
     }
 }
